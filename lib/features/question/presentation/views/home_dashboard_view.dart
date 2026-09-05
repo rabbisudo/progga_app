d0a47885338722086d618db1e4761cf174588a59
rabbisudo <rabbisudo@gmail.com>
2026-09-05 14:45:01 +0600

    fix(imports): add missing secureStorageServiceProvider import in home_dashboard_view.dart

--- lib/features/question/presentation/views/home_dashboard_view.dart
+++ lib/features/question/presentation/views/home_dashboard_view.dart
@@ -16,6 +16,7 @@ import '../widgets/spaced_repetition_widget.dart';
 import 'spaced_repetition_notifier.dart';
 import '../../../app_update/data/app_update_service.dart';
 import '../../../../core/services/notification_service.dart';
+import '../../../../core/storage/secure_storage_service.dart';
 
 final activeBannersProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
   final client = ref.watch(apiClientProvider);
