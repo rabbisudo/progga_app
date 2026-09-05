9be66335bd738da5ffe164d8e74fce2154fb466f
rabbisudo <rabbisudo@gmail.com>
2026-09-05 20:12:50 +0600

    fix(ios): enforce pure white window background in SceneDelegate for iOS 13+ devices

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,13 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-
+  override func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    super.scene(scene, willConnectTo: session, options: connectionOptions)
+    self.window?.backgroundColor = UIColor.white
+    self.window?.rootViewController?.view.backgroundColor = UIColor.white
+  }
 }
