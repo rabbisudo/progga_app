d355f8ad426d4f0418220c65a56dcd54663c3727
rabbisudo <rabbisudo@gmail.com>
2026-09-05 20:25:56 +0600

    fix(ios): force UIUserInterfaceStyle light in Info.plist, Main.storyboard and AppDelegate to completely eliminate native dark mode launch inversion

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 13.0, *) {
+      self.window?.overrideUserInterfaceStyle = .light
+    }
     self.window?.backgroundColor = UIColor.white
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -8,6 +8,9 @@ class SceneDelegate: FlutterSceneDelegate {
     options connectionOptions: UIScene.ConnectionOptions
   ) {
     super.scene(scene, willConnectTo: session, options: connectionOptions)
+    if #available(iOS 13.0, *) {
+      self.window?.overrideUserInterfaceStyle = .light
+    }
     self.window?.backgroundColor = UIColor.white
     self.window?.rootViewController?.view.backgroundColor = UIColor.white
   }
